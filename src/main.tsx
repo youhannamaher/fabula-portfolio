3b09659f680f07f8696f2ab6f4406cd7472731ad
youhannamaher <youhannamaher24@gmail.com>
2026-04-28 13:47:43 +0300

    Update portfolio Tue 04/28/2026 13:47:43.52

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FormspreeProvider } from '@formspree/react'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FormspreeProvider project="xojyorop">
+      <App />
+    </FormspreeProvider>
   </StrictMode>,
 )
